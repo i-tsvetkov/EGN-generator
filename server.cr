09d1c50bb766ca43f11effa2d9cdbb03d296b8e5
i-tsvetkov <i.tsvetkov.4@gmail.com>
2016-07-16 22:43:13 +0300

    server bind correctly

--- server.cr
+++ server.cr
@@ -1,6 +1,6 @@
 require "http/server"
 
-HTTP::Server.new("127.0.0.1", 8080, [
+HTTP::Server.new("0.0.0.0", 4444, [
   HTTP::ErrorHandler.new,
   HTTP::LogHandler.new,
   HTTP::StaticFileHandler.new(".")
