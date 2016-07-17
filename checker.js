d32f2b0f12d9ce063599ad2ef974f184d9d22d05
i-tsvetkov <i.tsvetkov.4@gmail.com>
2016-07-17 15:11:06 +0300

    added option for filtering EGNs by region

--- checker.js
+++ checker.js
@@ -1,5 +1,37 @@
 (function () {
   var btn, egn, tbl;
+  var digits = _.range(10);
+  var regions = {
+    "Благоевград":       {"min":"000", "max":"043"},
+    "Бургас":            {"min":"044", "max":"093"},
+    "Варна":             {"min":"094", "max":"139"},
+    "Велико Търново":    {"min":"140", "max":"169"},
+    "Видин":             {"min":"170", "max":"183"},
+    "Враца":             {"min":"184", "max":"217"},
+    "Габрово":           {"min":"218", "max":"233"},
+    "Кърджали":          {"min":"234", "max":"281"},
+    "Кюстендил":         {"min":"282", "max":"301"},
+    "Ловеч":             {"min":"302", "max":"319"},
+    "Монтана":           {"min":"320", "max":"341"},
+    "Пазарджик":         {"min":"342", "max":"377"},
+    "Перник":            {"min":"378", "max":"395"},
+    "Плевен":            {"min":"396", "max":"435"},
+    "Пловдив":           {"min":"436", "max":"501"},
+    "Разград":           {"min":"502", "max":"527"},
+    "Русе":              {"min":"528", "max":"555"},
+    "Силистра":          {"min":"556", "max":"575"},
+    "Сливен":            {"min":"576", "max":"601"},
+    "Смолян":            {"min":"602", "max":"623"},
+    "София - град":      {"min":"624", "max":"721"},
+    "София - окръг":     {"min":"722", "max":"751"},
+    "Стара Загора":      {"min":"752", "max":"789"},
+    "Добрич (Толбухин)": {"min":"790", "max":"821"},
+    "Търговище":         {"min":"822", "max":"843"},
+    "Хасково":           {"min":"844", "max":"871"},
+    "Шумен":             {"min":"872", "max":"903"},
+    "Ямбол":             {"min":"904", "max":"925"},
+    "Друг/Неизвестен":   {"min":"926", "max":"999"}
+  };
 
   var cartesian = (arr, n) => {
     if (n === 0 || n < 0)
@@ -38,8 +70,18 @@
   var genderOk = (gender) => (egn) => (gender === "M") ? egn[8] % 2 === 0
                                     : (gender === "F") ? egn[8] % 2 !== 0
                                                        : true;
-
-  var digits = _.range(10);
+  var regionOk = (region) => {
+    if (regions[region]) {
+      var min = Number(regions[region].min),
+          max = Number(regions[region].max);
+      return (egn) => {
+        var n = Number(_.take(_.drop(egn, 6), 3).join(''));
+        return min <= n && n <= max;
+      };
+    }
+    else
+      return (egn) => true;
+  };
 
   var fillPos = (arr, item, n) => _.take(arr, n)
                                   .concat([item])
@@ -58,7 +100,8 @@
     egns
     .filter(genderOk(getGender()))
     .filter(egnOk)
-    .filter(e => moment(_.take(e, 6).join(''), 'YYMMDD').isValid());
+    .filter(e => moment(_.take(e, 6).join(''), 'YYMMDD').isValid())
+    .filter(regionOk(getRegion()));
 
   var findEgns = (egn) =>
     filterEgns(
@@ -85,6 +128,7 @@
       tbl.insertRow().insertCell().textContent = r.join(""));
 
   var getGender = () => document.getElementById("gender").value;
+  var getRegion = () => document.getElementById("region").value;
 
   window.onload = () => {
     btn = document.getElementById("btn");
